360f4f2d01f93fcb0e4459d17d5d63e3018a842f
Rockacancun <gaston.hikuri@gmail.com>
2020-08-01 14:06:28 -0500

    clean code changes

--- views/board.py
+++ views/board.py
@@ -1,25 +1,28 @@
 from tkinter import Canvas
 
+
 class Board(Canvas):
 
     def __init__(self, parent, matrix, **kargs):
         self.__matrix = matrix
         self.__zoom = 20
         self.__observer = None
-        super().__init__(parent, width=1, height=1, highlightthickness=0, **kargs)
-        self.bind( "<Button-1>", self.paint )
+        super().__init__(parent, width=1, height=1,
+                         highlightthickness=0, **kargs)
+        self.bind("<Button-1>", self.paint)
         self.draw_canvas()
         self.draw_grid()
         self.print_cells()
-        
+
     def print_cells(self):
-        for column in range(len(self.__matrix)):
-            for row in range(len(self.__matrix)):
+        matrix_size = self.matrix_size()
+        for column in range(matrix_size):
+            for row in range(matrix_size):
                 self.print_live_or_dead_cell(column, row)
 
     def print_live_or_dead_cell(self, column, row):
         if self.__matrix[column][row] == 0:
-            self.print_dead_cell(column, row )
+            self.print_dead_cell(column, row)
             return
         self.print_live_cell(column, row)
 
@@ -34,7 +37,8 @@ class Board(Canvas):
         origin_y = y * self.__zoom + y
         destiny_x = origin_x + self.__zoom - 1
         destiny_y = origin_y + self.__zoom - 1
-        self.create_rectangle(origin_x, origin_y, destiny_x, destiny_y, fill=color, width=0)
+        self.create_rectangle(origin_x, origin_y, destiny_x,
+                              destiny_y, fill=color, width=0)
 
     def update_matrix(self, matrix):
         self.__matrix = matrix
@@ -49,27 +53,41 @@ class Board(Canvas):
         self.draw_grid_axis_y()
 
     def draw_grid_axis_x(self):
-       for column in range(self.get_grid_pixels()):
+        for column in range(self.get_grid_pixels()):
             cell_pixels = self.__zoom * (column+1)
             grid_pixels = column
             column_to_paint = cell_pixels + grid_pixels
-            self.create_line(column_to_paint, 0, column_to_paint, self.calculate_canvas_pixels(), fill="#6C7883", width=1)
+            height_zero = 0
+            self.create_line(
+                column_to_paint,
+                height_zero,
+                column_to_paint,
+                self.calculate_canvas_pixels(),
+                fill="#6C7883",
+                width=1)
 
     def draw_grid_axis_y(self):
-       for row in range(self.get_grid_pixels()):
+        for row in range(self.get_grid_pixels()):
             cell_pixels = self.__zoom * (row+1)
             grid_pixels = row
             row_to_paint = cell_pixels + grid_pixels
-            self.create_line(0, row_to_paint, self.calculate_canvas_pixels(), row_to_paint , fill="#6C7883", width=1)
+            position_zero = 0
+            self.create_line(
+                position_zero,
+                row_to_paint,
+                self.calculate_canvas_pixels(),
+                row_to_paint,
+                fill="#6C7883",
+                width=1)
 
     def calculate_canvas_pixels(self):
         return self.get_cell_pixels() + self.get_grid_pixels()
 
     def get_grid_pixels(self):
-        return len(self.__matrix) - 1 
+        return self.matrix_size() - 1
 
     def get_cell_pixels(self):
-        return len(self.__matrix) * self.__zoom
+        return self.matrix_size() * self.__zoom
 
     def register_touch_cell_observer(self, observer):
         self.__observer = observer
@@ -80,10 +98,6 @@ class Board(Canvas):
         cell_index_x = self.calculate_cell_index(x)
         cell_index_y = self.calculate_cell_index(y)
 
-        print("x, %s" % cell_index_x)
-        print("y, %s" % cell_index_y)
-
-
         if(self.__observer):
             self.__observer(cell_index_x, cell_index_y)
 
@@ -92,4 +106,5 @@ class Board(Canvas):
         pixels_without_grid_pixel = pixel - grid_pixels_to_discount
         return pixels_without_grid_pixel // self.__zoom
 
-
+    def matrix_size(self):
+        return len(self.__matrix)
--- views/conway_frame.py
+++ views/conway_frame.py
@@ -4,10 +4,10 @@ from tkinter import Button
 from tkinter import PhotoImage
 from views.board import Board
 from tkinter.constants import BOTH, BOTTOM, X, LEFT, CENTER, TRUE
-from random import randrange
+
 
 class ConwayFrame(Frame):
-    
+
     def __init__(self, parent):
         super().__init__(parent, bg="#0E1621")
         self.__matrix = [[0 for x in range(10)] for y in range(10)]
@@ -21,13 +21,13 @@ class ConwayFrame(Frame):
 
         canvas_frame.pack(fill=X, expand=TRUE)
         action_frame.pack(fill=X, side=BOTTOM)
-        info_frame.pack(fill=X, side=BOTTOM )
-        
+        info_frame.pack(fill=X, side=BOTTOM)
+
         self.pack(fill=BOTH, expand=TRUE)
 
     def create_canvas_frame(self):
         canvas_frame = Frame(self, bg="#0E1621")
-        
+
         self.__board = Board(canvas_frame, self.__matrix, bg="#242F3D")
         self.__board.register_touch_cell_observer(self.touch_event_hadler)
         self.__board.place(relx=0.5, rely=0.5, anchor=CENTER)
@@ -35,7 +35,8 @@ class ConwayFrame(Frame):
         return canvas_frame
 
     def touch_event_hadler(self, cell_index_x, cell_index_y):
-        self.__matrix[cell_index_x][cell_index_y] = 1 if self.__matrix[cell_index_x][cell_index_y] == 0 else 0
+        cell = self.__matrix[cell_index_x][cell_index_y]
+        self.__matrix[cell_index_x][cell_index_y] = 1 if cell == 0 else 0
         self.__board.update_matrix(self.__matrix)
 
     def create_info_frame(self):
@@ -43,9 +44,23 @@ class ConwayFrame(Frame):
 
         center_frame = Frame(info_frame, bg="#17212B")
 
-        generation_label = Label(center_frame, bg="#17212B", fg="white", text='Generation:',)
-        life_death_label = Label(center_frame, bg="#17212B", fg="white", text='Live: 0 - Dead: 0')
-        speed_label = Label(center_frame, bg="#17212B", fg="white", text='Speed: 1x')
+        generation_label = Label(
+            center_frame,
+            bg="#17212B",
+            fg="white",
+            text='Generation:')
+
+        life_death_label = Label(
+            center_frame,
+            bg="#17212B",
+            fg="white",
+            text='Live: 0 - Dead: 0')
+
+        speed_label = Label(
+            center_frame,
+            bg="#17212B",
+            fg="white",
+            text='Speed: 1x')
 
         generation_label.grid(row=0, column=0, padx=10, pady=10)
         life_death_label.grid(row=0, column=1, padx=10, pady=10)
@@ -56,63 +71,115 @@ class ConwayFrame(Frame):
         return info_frame
 
     def create_actions_frame(self):
-        actions_frame = Frame(self, bg="#17212B")
+        frame = Frame(self, bg="#17212B")
 
-        actions_secondary_frame = self.create_secondary_actions_frame(actions_frame)
-        actions_secondary_frame.pack(side = LEFT)
+        actions_secondary_frame = self.create_secondary_actions_frame(frame)
+        actions_secondary_frame.pack(side=LEFT)
 
-        actions_primary_frame = self.create_primary_actions_frame(actions_frame)
+        actions_primary_frame = self.create_primary_actions_frame(frame)
         actions_primary_frame.pack()
 
-        return actions_frame
+        return frame
 
     def create_secondary_actions_frame(self, parent_frame):
-        secondary_actions_frame = Frame(parent_frame, bg="#17212B")
-        
-        photo_random = PhotoImage(file="resources/random.png")
-        random_button = Button(secondary_actions_frame, text="Random", image=photo_random, fg="#6C7883", command=self.action)
-        random_button.image = photo_random
+        frame = Frame(parent_frame, bg="#17212B")
 
-        photo_grid = PhotoImage(file="resources/grid.png")
-        grid_button = Button(secondary_actions_frame, text="Grid", image=photo_grid, fg="#6C7883", command=self.action)
-        grid_button.image = photo_grid
-
-        photo_photo = PhotoImage(file="resources/photo.png")
-        photo_button = Button(secondary_actions_frame, text="Photo", image=photo_photo, fg="#6C7883", command=self.action)
-        photo_button.image = photo_photo
+        random_button = self.create_random_button(frame)
+        grid_button = self.create_grid_button(frame)
+        photo_button = self.create_photo_button(frame)
 
         random_button.grid(row=0, column=1, padx=10, pady=10)
         grid_button.grid(row=0, column=2, padx=10, pady=10)
         photo_button.grid(row=0, column=3, padx=10, pady=10)
 
-        return secondary_actions_frame
+        return frame
+
+    def create_random_button(self, parent):
+        image = PhotoImage(file="resources/random.png")
+        button = Button(
+            parent,
+            text="Random",
+            image=image,
+            fg="#6C7883",
+            command=self.action)
+        button.image = image
+        return button
+
+    def create_grid_button(self, parent):
+        image = PhotoImage(file="resources/grid.png")
+        grid_button = Button(
+            parent,
+            text="Grid",
+            image=image,
+            fg="#6C7883",
+            command=self.action)
+        grid_button.image = image
+        return grid_button
+
+    def create_photo_button(self, parent):
+        photo = PhotoImage(file="resources/photo.png")
+        button = Button(
+            parent,
+            text="Photo",
+            image=photo,
+            fg="#6C7883",
+            command=self.action)
+        button.image = photo
+        return button
 
     def create_primary_actions_frame(self, parent_frame):
-        primary_actions= Frame(parent_frame, bg="#17212B")
-
-        photo_stop = PhotoImage(file="resources/stop.png")
-        stop_button = Button(primary_actions, text="Stop", image=photo_stop, fg="#6C7883", command=self.action)
-        stop_button.image = photo_stop
-
-        photo_speed_up = PhotoImage(file="resources/speed_up.png")
-        speed_up_button = Button(primary_actions, text="Speed Up", image=photo_speed_up, fg="#6C7883", command=self.action)
-        speed_up_button.image = photo_speed_up
-
-        photo_play = PhotoImage(file="resources/play.png")
-        play_button = Button(primary_actions, text="Play", image=photo_play, fg="#6C7883", command=self.action)
-        play_button.image = photo_play
-
-        photo_speed_down = PhotoImage(file="resources/speed_down.png")
-        speed_down = Button(primary_actions, text="Speed Down", image=photo_speed_down, fg="#6C7883", command=self.action)
-        speed_down.image = photo_speed_down
-
+        frame = Frame(parent_frame, bg="#17212B")
+        stop_button = self.create_stop_button(frame)
+        speed_up_button = self.create_speed_button_up(frame)
+        play_button = self.create_play_button(frame)
+        speed_down_button = self.create_speed_down_button(frame)
 
         stop_button.grid(row=0, column=1, padx=10, pady=10)
-        speed_up_button.grid(row=0, column=2, padx=10, pady=10 )
-        play_button.grid(row=0, column=3, padx=10, pady=10 )
-        speed_down.grid(row=0, column=4, padx=10, pady=10 )
+        speed_up_button.grid(row=0, column=2, padx=10, pady=10)
+        play_button.grid(row=0, column=3, padx=10, pady=10)
+        speed_down_button.grid(row=0, column=4, padx=10, pady=10)
 
-        return primary_actions
+        return frame
+
+    def create_stop_button(self, parent_frame):
+        photo = PhotoImage(file="resources/stop.png")
+        button = Button(parent_frame,
+                        text="Stop",
+                        image=photo,
+                        fg="#6C7883",
+                        command=self.action)
+        button.image = photo
+        return button
+
+    def create_speed_button_up(self, parent_frame):
+        photo = PhotoImage(file="resources/speed_up.png")
+        button = Button(parent_frame,
+                        text="Speed Up",
+                        image=photo,
+                        fg="#6C7883",
+                        command=self.action)
+        button.image = photo
+        return button
+
+    def create_play_button(self, parent_frame):
+        photo = PhotoImage(file="resources/play.png")
+        button = Button(parent_frame,
+                        text="Play",
+                        image=photo,
+                        fg="#6C7883",
+                        command=self.action)
+        button.image = photo
+        return button
+
+    def create_speed_down_button(self, parent):
+        photo = PhotoImage(file="resources/speed_down.png")
+        button = Button(parent,
+                        text="Speed Down",
+                        image=photo,
+                        fg="#6C7883",
+                        command=self.action)
+        button.image = photo
+        return button
 
     def action(self):
         pass
